fd69b7cd863ac462f14b4c73ddc81d3b11d197ea
Matthew Young <myoung@wehi.edu.au>
2010-06-03 06:10:51 +0000

    The spline fitting function was properly being constrained to be strictly positive.  Added a dummy entry to covariates at 0 to force the constraint.  The dummy entry should have negligable effect for any reasonabley sized dataset.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/goseq@47266 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/makespline.R
+++ R/makespline.R
@@ -1,5 +1,7 @@
 makespline=function (x, y, newX=NULL, nKnots = 6) 
 {
+    #Should not be used outside of goseq package.  Refer to the help pages for pcls in the mgcv package for more general 
+    #contstrained spline fitting.
 	ww=order(x)
 	size=ceiling(length(y)/10)
 	low=sum(y[ww][1:size])
@@ -13,12 +15,23 @@ makespline=function (x, y, newX=NULL, nKnots = 6)
     nKnots <- round(nKnots)
     if (is.null(newX)) 
         newX <- x
-   f.ug <- gam(y ~ s(x, k = nKnots, bs = "cr"),family=binomial())
+    #We need to force the fit to go through (0,0), mono.con constructs a requirment that the LOWEST VALUE go through 0
+    #In order to make the lowest value 0, we add in a dummy point.  Hopefully this does not skew things too greatly
+    #We use an inequality constraint rather than equality to only skew the fit enough that the spline is always >0
+    #This won't work if we have a monotonically decreasing function, so check that
+    if(hi>low){
+	    x=c(0,x)
+	    y=c(0,y)
+    }
+    f.ug <- gam(y ~ s(x, k = nKnots, bs = "cr"),family=binomial())
     dat <- data.frame(x = x, y = y)
     sm <- smoothCon(s(x, k = nKnots, bs = "cr"), dat, knots = NULL)[[1]]
     if (length(sm$xp) < 6) 
         warning("Few than 6 nKnots were specified!\n Possible inaccurate fitting!\n")
-    F <- mono.con(sm$xp,TRUE,lower=0,upper=1)
+    #The lower and upper parameters here don't seem to build the right constraints 100% of the time, so add them manually
+    F <- mono.con(sm$xp,TRUE)
+    F$A=rbind(F$A,c(1,rep(0,ncol(F$A)-1)),c(rep(0,ncol(F$A)-1),-1))
+    F$b=c(F$b,0,1)
     G <- list(X = sm$X, C = matrix(0, 0, 0), sp = f.ug$sp, p = sm$xp, 
         y = y, w = y * 0 + 1, Ain =F$A, bin = F$b, S = sm$S, 
         off = 0)
