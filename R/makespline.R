67d8adace7610421613ded78edf7c4eecbd5199e
Matthew Young <myoung@wehi.edu.au>
2010-06-08 06:16:23 +0000

    If the pcls fitting procedure still fails to satisfy the constraints and produce a PWF through (0,0), then add a minor offset to everything to ensure all gene weights are positive.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/goseq@47363 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/makespline.R
+++ R/makespline.R
@@ -2,6 +2,7 @@ makespline=function (x, y, newX=NULL, nKnots = 6)
 {
     #Should not be used outside of goseq package.  Refer to the help pages for pcls in the mgcv package for more general 
     #contstrained spline fitting.
+    	#We handle montonocily decreasing problems by reformulating them as monotonicly increasing by reflecting about "infinity"
 	ww=order(x)
 	size=ceiling(length(y)/10)
 	low=sum(y[ww][1:size])
@@ -15,10 +16,10 @@ makespline=function (x, y, newX=NULL, nKnots = 6)
     nKnots <- round(nKnots)
     if (is.null(newX)) 
         newX <- x
-    #We need to force the fit to go through (0,0), mono.con constructs a requirment that the LOWEST VALUE go through 0
-    #In order to make the lowest value 0, we add in a dummy point.  Hopefully this does not skew things too greatly
+    #We need to force the fit to go through (0,0), mono.con constructs a requirment that the LOWEST x VALUE be greater than 0
+    #so to force the fit through (0,0) we add a fake lowest x value of 0 by adding in a dummy point. 
     #We use an inequality constraint rather than equality to only skew the fit enough that the spline is always >0
-    #This won't work if we have a monotonically decreasing function, so check that
+    #This won't work if we have a monotonically decreasing function (and we don't need to it), so check that
     if(hi>low){
 	    x=c(0,x)
 	    y=c(0,y)
@@ -38,5 +39,11 @@ makespline=function (x, y, newX=NULL, nKnots = 6)
     p <- pcls(G)
     fv <- Predict.matrix(sm, data.frame(x = newX)) %*% p
     fv <- as.vector(fv)
-   return(fv)
+    #If pcls still fails for some reason and returns negative values (or values that are so low that they'll have an effective relative weight of Inf
+    #then we need to add a little bit to every weight to make things non-negative, the choice of minimum weight is somewhat arbitrary and serves only to
+    #ensure that we don't have positive weights that will be infinitely prefered as a ratio.
+    lower_bound=10^-3
+    if(min(fv)<lower_bound)
+    	fv=fv-min(fv)+lower_bound
+    return(fv)
 }
