fed9d659b9bde81e6f0db8510135456d77ed0a9f
Nadia Davidson <nadia.davidson@mcri.edu.au>
2013-01-31 06:21:46 +0000

    Added functionality to getlength.R to attempt to retreive gene lengths from UCSC if they are not present in genLenDataBase
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/goseq@73035 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getlength.R
+++ R/getlength.R
@@ -1,8 +1,8 @@
 #############################################################################
 #Description: Fetches gene length data for the genome and id specified.
 #Notes:
-#Author: Matthew Young
-#Date Modified: 13/12/2010
+#Author: Matthew Young, Nadia Davidson
+#Date Modified: 31/1/2013
 
 getlength=function(genes,genome,id){
 	#geneLenDataBase contains all the gene length data, check it is installed
@@ -13,27 +13,79 @@ getlength=function(genes,genome,id){
 	repo=grep(".*\\..*\\.LENGTH",as.data.frame(data(package="geneLenDataBase")$results,stringsAsFactors=FALSE)$Item,ignore.case=TRUE,value=TRUE)
 	#Is this genome/id in the local repository?
 	if(paste(genome,id,"LENGTH",sep='.')%in%repo){
-		#Yes it is, so load it and group transcript lengths into genes
-		message("Loading ",genome," length data...")
-		data(list=paste(genome,id,"LENGTH",sep='.'),package='geneLenDataBase')
-		data=split(get(paste(genome,id,"LENGTH",sep='.'))$Length,get(paste(genome,id,"LENGTH",sep='.'))$Gene)
-	}else{
+	   #Yes it is, so load it and group transcript lengths into genes
+	   message("Loading ",genome," length data...")
+	   data(list=paste(genome,id,"LENGTH",sep='.'),package='geneLenDataBase')
+	   data=split(get(paste(genome,id,"LENGTH",sep='.'))$Length,get(paste(genome,id,"LENGTH",sep='.'))$Gene)
+
+	   #Get the names of the genes used in the genome package
+	   gene_names=get(paste(genome,id,"LENGTH",sep="."))$Gene
+	} else {
+	   
+	   #If it's not in geneLenDataBase, try to load the data from UCSC using rtracklayer
+	   if(!require("rtracklayer")){
+		stop("Can't load the rtracklayer library. Is it installed?...")
+	   }
+		message("Can't find ",genome,"/",id," length data in genLenDataBase... Trying to download from UCSC. ",
+			 "This might take a couple of minutes. ")
+
+	   tryCatch( {
+			 
 		#If not, then the user will have to supply it themselves
-		stop("Length information for genome ",genome," and gene ID ",id," is not in the geneLenDataBase database.  You will have to specify bias.data manually.")
+		#download the relevant table from UCSC
+		session <- browserSession()
+		genome(session)<-genome
+		query <- ucscTableQuery(session,id)
+		transTable<-getTable(query)
+
+		# all this below is to get the length information of each gene
+		starts=strsplit(as.character(transTable$exonStart),",")
+		ends=strsplit(as.character(transTable$exonEnd),",")
+		exons=mapply(data.frame, starts, ends, SIMPLIFY=FALSE)
+		get_length<-function(x){ 
+		   sum(as.integer(as.character(x[,2]))-as.integer(as.character(x[,1])))  
+                }
+		lengths=sapply(exons,get_length)
+
+		# now we need to get the correct list of gene names.
+		names1=as.character(transTable$name)
+		gene_names=names1
+		if(!any(genes%in%names1)){   
+		   #check which column has the gene names
+		   names2=as.character(transTable$name2)    
+		   if(any(genes%in%names2)){
+			gene_names=names2
+		   }
+		}
+		data<-split(lengths,gene_names)
+
+                }, 
+		error=function(e){ 
+		   stop("Length information for genome ",genome," and gene ID ",id,
+			" is not available. You will have to specify bias.data manually.") 
+		}
+            )
 	}
-	#In order to assign just one length to each gene, we store the Median, Min Max and count of transcripts for each gene for later use
-	len=data.frame(Gene=names(data),Median=sapply(data,median),Min=sapply(data,min),Max=sapply(data,max),Count=sapply(data,length))
-	#Get the names of the genes used in the genome package
-	gene_names=get(paste(genome,id,"LENGTH",sep="."))$Gene
-	#How many of them match the names the user specified with "genes"
+
+	#In order to assign just one length to each gene, we store the Median, Min Max 
+	#and count of transcripts for each gene for later use
+	len=data.frame(Gene=names(data),Median=sapply(data,median),Min=sapply(data,min),
+			Max=sapply(data,max),Count=sapply(data,length))
+
+	#How many of the genes match the names the user specified with "genes"
 	matched_frac=sum(genes%in%gene_names)/length(genes)
 	#None? Throw an error.
 	if(matched_frac==0){
-		stop("The gene names specified do not match the gene names for genome ",genome," and ID ",id,".\n\tGene names given were: ",paste(genes[1:10],collapse=", "),"\n\tRequired gene names are: ",paste(gene_names[1:10],collapse=", "))
+		stop("The gene names specified do not match the gene names for genome ",
+		genome," and ID ",id,".\n\tGene names given were: ",paste(genes[1:10],collapse=", "),
+		"\n\tRequired gene names are: ",paste(gene_names[1:10],collapse=", "))
 	}
 	#Less than 60%? Give a warning and continue
 	if(matched_frac<.6){
-		warning("More than 40% of gene names specified did not match the gene names for genome ",genome," and ID ",id,".  No length data will be available for these genes.\n\tGene names which failed to match were: ",paste(genes[!genes%in%gene_names][1:10],collapse=", "),"\n\tRequired gene names are: ",paste(gene_names[1:10],collapse=", "))
+		warning("More than 40% of gene names specified did not match the gene names for genome ",
+		genome," and ID ",id,".  No length data will be available for these genes.\n\tGene names which failed to match were: ",
+		paste(genes[!genes%in%gene_names][1:10],collapse=", "),"\n\tRequired gene names are: ",
+		paste(gene_names[1:10],collapse=", "))
 	}
 	#Finally, return the median transcript length for each matching gene
 	return(len$Median[match(genes,len$Gene)])
